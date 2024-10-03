ec2622869202904ab691ba67cd475fd680b2914e
swastikaaaaa <swastikabhattarai29@gmail.com>
2024-10-03 22:54:20 +0545

    Descriptive commit message about the changes

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const index = require('./Routes/routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerJsdoc = require('./swaggerconfig');
 require('dotenv').config();
-require('dotenv').config();
-app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 app.use((req, res, next) => {
@@ -17,7 +15,6 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
-
 
 
 app.use('/index/details',index);
@@ -36,13 +33,14 @@ app.get("/",(req,res) => {
 })
 // server banyo
 
-MONGO_URI = "mongodb+srv://dalley_user_20599:dalley_user_20599@cluster0.7cf3b.mongodb.net/"
 
 
 const start = async () => 
     {
         try
         {
+            const MONGO_URI = process.env.MONGO_URI;
+
             await connectdb(MONGO_URI);
             //const client = new MongoClient(URL);
             app.listen(port,console.log(`Server is listening on port ${port}`));
