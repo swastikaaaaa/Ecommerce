13baf17cc6528a61c6e39195d56d19ffb7dd9436
swastikaaaaa <swastikabhattarai29@gmail.com>
2024-10-06 00:19:21 +0545

    Routes

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,13 +1,17 @@
 const express=require('express');
 const router=express.Router();
-const{PostProduct,UpdateProduct,GetAllCategory,PostAllCategory,}=require('../Controller/controller');
+const{PostProduct,UpdateProduct,GetAllCategory,PostAllCategory,PostUser,UserInfo}=require('../Controller/controller');
 
 router.route('/Product/').post(PostProduct);
 router.route('/Product/:PrdId').put(UpdateProduct);
 router.route('/getallcartdetails').get(GetAllCategory);
 router.route('/postcartdetails').post(PostAllCategory);
+router.route('/login').post(UserInfo);
+router.route('./register').post(PostUser);
 
 module.exports=router;
+
+
 
 
 
