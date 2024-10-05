73c4297af424c4eeea89e64143c276c83e8c7f74
swastikaaaaa <swastikabhattarai29@gmail.com>
2024-10-06 00:26:31 +0545

    start server

--- index.js
+++ index.js
@@ -26,28 +26,35 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc));
 
 
 
-
+//MongooseError: The `uri` parameter to `openUri()` must be a string, got "undefined". Make sure the first parameter to `mongoose.connect()` or `mongoose.createConnection()` is a string.err  
 app.get("/",(req,res) => {
     // console.log(request.body)
     res.send("Hello")
 })
 // server banyo
 
+const MONGO_URI = process.env.MONGO_URI;
+
+function start()
 
 
-const start = async () => 
+{
+    try
     {
-        try
-        {
-            const MONGO_URI = process.env.MONGO_URI;
+        const start = async () => 
+        await connectdb(process.env.MONGO_URI);
+        //const client = new MongoClient(URL);
+        app.listen(port,console.log(`Server is listening on port ${port}`));
+        start();
+        //console.log("MONGO_URI:", MONGO_URI);
+        
+    }
+    catch(err){console.log(err)}
+}
 
-            await connectdb(MONGO_URI);
-            //const client = new MongoClient(URL);
-            app.listen(port,console.log(`Server is listening on port ${port}`));
-                    
-                }
-                catch(err){console.log(err)}
-            }
-            
-            start();
+
+start();
+
+
+
 
